54bebff59b5ed97a5d85c617720086582524d38a
Haibara777 <43092374+Haibara777@users.noreply.github.com>
2019-01-22 00:30:18 +0800

    Update scale.fix.js

--- assets/js/scale.fix.js
+++ assets/js/scale.fix.js
@@ -25,3 +25,8 @@
         document.addEventListener("touchend", gestureEnd, false);
     }
 })(document);
+<script type="text/javascript"
+	src="//cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML,
+	/javascripts/MathJaxLocal.js
+">
+</script>
